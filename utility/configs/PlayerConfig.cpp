1522febf8dcbaedaecfe4b2d96cb691f854d5e1b
Bobrov Oleg <olegich051@gmail.com>
2020-01-21 14:39:12 +0300

    new playerConfig

--- utility/configs/PlayerConfig.cpp
+++ utility/configs/PlayerConfig.cpp
@@ -2,17 +2,28 @@
 #include <fstream>
 #include "../json.hpp"
 
+nlohmann::json PlayerConfig::login;
 std::string PlayerConfig::playerName;
 std::string PlayerConfig::hostName;
 size_t PlayerConfig::numPlayers;
+size_t PlayerConfig::numTurns;
 
 PlayerConfig::PlayerConfig() {
     std::ifstream configFile("playerConfig.json");
-    nlohmann::json playerConfig;
-    configFile >> playerConfig;
-    PlayerConfig::hostName = playerConfig["hostName"];
-    PlayerConfig::numPlayers = playerConfig["numPlayers"];
-    PlayerConfig::playerName = playerConfig["playerName"];    
+    configFile >> login;
+    if (login.contains("name")) {
+        PlayerConfig::playerName = login["name"];
+    }
+    if (login.contains("game")) {
+        PlayerConfig::hostName = login["game"];
+    }
+    if (login.contains("num_players")) {
+        PlayerConfig::numPlayers = login["num_players"];
+    }
+    if (login.contains("num_turns")) {
+        PlayerConfig::numTurns = login["num_turns"];
+    }
+
 }
 
 PlayerConfig PlayerConfig::playerConfigInit;
--- utility/configs/PlayerConfig.hpp
+++ utility/configs/PlayerConfig.hpp
@@ -2,14 +2,17 @@
 #define PLAYERCONFIG_HPP
 
 #include <string>
+#include "../json.hpp"
 
 class PlayerConfig
 {
 public:
     PlayerConfig();
+    static nlohmann::json login;
     static std::string playerName;
     static std::string hostName;
     static size_t numPlayers;
+    static size_t numTurns;
     // trick to init config from file
     static PlayerConfig playerConfigInit;
 };
